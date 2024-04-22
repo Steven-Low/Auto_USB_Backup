2fff1a372306fd52174d512214db7f625d8f36b9
Steven-Low <2002lowsy@gmail.com>
2024-04-22 22:11:50 +0800

    rename cmd

--- install.cmd
+++ install.cmd
@@ -2,5 +2,5 @@ REM Get the directory of the batch script
 set "script_dir=%~dp0"
 REM Create a basic task named "DriverFrameworks Task"
 
-schtasks /create /tn "AutoUSBBackup2" /sc onevent /ec "Microsoft-Windows-DriverFrameworks-UserMode/Operational" /mo "*[System/EventID=2101]" /tr %script_dir%nothing.cmd 
+schtasks /create /tn "AutoUSBBackup2" /sc onevent /ec "Microsoft-Windows-DriverFrameworks-UserMode/Operational" /mo "*[System/EventID=2101]" /tr %script_dir%executepy.cmd 
 echo Task "AutoUSBbackup2" created successfully.
